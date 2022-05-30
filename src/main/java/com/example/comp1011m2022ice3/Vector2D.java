27adf798910474c0ea8111fb9b414e53bdf1d83d
nikunjpatel9426 <npatel43210@gmail.com>
2022-05-30 13:57:14 -0400

    first time

--- src/main/java/com/example/comp1011m2022ice3/Vector2D.java
+++ src/main/java/com/example/comp1011m2022ice3/Vector2D.java
@@ -1,5 +1,7 @@
 package com.example.comp1011m2022ice3;
 
+import java.util.Vector;
+
 public class Vector2D {
 
 
@@ -7,18 +9,59 @@ public class Vector2D {
     private int m_id;
     private float m_x;
     private float m_y;
+    // Constructors
 
-
-    public Vector2D() {
+    /**
+     * Default constructor
+     * - set x and y components to 0.0f and id to -1
+     */
+    public Vector2D(float x, float y) {
+    m_id = -1;
+    setX(0.0f);
+    setY(0.0f);
     }
 
-    //Accessors and mutators Methods
+    /**
+     * This constructor takes x and y parameters
+     * - id is set to -1
+     * @param x
+     * @param y
+     */
+    public void vector2D(float x, float y)
+    {
+        m_id = -1;
+        setX(x);
+        setY(y);
+    }
+
+    public Vector2D(int id, float x, float y)
+    {
+        this(x,y);
+        setID(id);
+    }
+
+    //public properties (getters and setters)
+
     public int getID() {
         return m_id;
     }
-
     public void setID(int id) {
-        m_id = id;
+        if (id < 0) {
+            throw new IllegalArgumentException ("vector ID must be greater than Zero");
+        } else {
+            m_id = id;
+        }
+    }
+
+
+    public float getY()
+    {
+        return m_y;
+    }
+
+    public void setY(float new_y)
+    {
+        m_y = new_y;
     }
 
     public float getX() {
@@ -29,36 +72,41 @@ public class Vector2D {
         m_x=new_x;
     }
 
-    public float getY() {
-        return m_y;
-    }
-
-    public void setY(float new_y) {
-        m_y=new_y;
-    }
 
 
-    //Constrcuctor
-    public void Vector2D() {
-        m_x = 0.0f;
-        m_y = 0.0f;
-    }
+    // Public Methods
 
-    public Vector2D(float x, float y) {
-        m_x = x;
-        m_y = y;
-    }
-
-    public Vector2D add(Vector2D vector) {
-
+    /**
+     * This method adds a Vector2D to this Vector2D
+     *
+     * @param vector
+     * @return
+     */
+    public Vector2D add(Vector2D vector)
+    {
         m_x += vector.getX();
         m_y += vector.getY();
         return this;
     }
 
-    public Vector2D substrct(Vector2D vector) {
-        m_x = vector.getX()-m_x;
-        m_y += vector.getY()-m_y;
+    /**
+     * @param vector
+     * @return
+     */
+
+    public Vector2D subtract(Vector2D vector) {
+
+        m_x += vector.getX() - m_x;
+        m_y += vector.getY() - m_y;
+        return this;
+    }
+
+    /**
+     * @return
+     */
+    public Vector2D substrct(Vector2D Vector) {
+        m_x = Vector.getX()-m_x;
+        m_y += Vector.getY()-m_y;
         return this;
     }
 
